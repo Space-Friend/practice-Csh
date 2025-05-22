77677c323506d3cf16b7ab3325c1e645c204e8ac
Space <Space@Fr>
2025-05-22 15:50:07 +0300

    Работает клянусь

--- Test3/Program.cs
+++ Test3/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int[][] jaggedArr = new int[][]
+﻿int[][] jaggedArr = new int[][]
 {
   new int[ ] {1,8,2,7,9},
   new int[ ] {2,4,6},
@@ -8,10 +6,18 @@ int[][] jaggedArr = new int[][]
 };
 
 string[][] fruits = { new string[] { "Apple", "Apricot" }, new string[] { "Mango", "Orange", "Melon" } };
-for (int i; i<2; i++)
+for (int i = 0; i < 3; i++)
 {
-    for (int j; j<3; j++)
+    for (int j = 0; j < 5; j++)
     {
-        Console.WriteLine(fruits[i][j]);
+        try
+        {
+            Console.WriteLine(jaggedArr[i][j] + " i={0}, j={1}; ", i, j);
+
+        }
+        catch
+        {
+            Console.WriteLine("  Failed! i={0}, j={1}; ", i, j);
+        }
     }
 }
