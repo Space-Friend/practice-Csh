1d07f7ccae5520c3124b632b5effddd8bd2d7114
Space-Friend <Makashev_Leonid@mail.ru>
2025-05-23 10:11:30 +0300

    Program, Main

--- Test3/Arrays.cs
+++ Test3/Arrays.cs
@@ -1,21 +1,27 @@
-﻿int[][] jaggedArr =
-[
-  [1,2,3,4],
-  [2,2,3,33]
-];
-
-string[][] fruits = { new string[] { "Apple", "Apricot" }, new string[] { "Mango", "Orange", "Melon" } };
-for (int i = 0; i < 3; i++)
+﻿internal class Program
 {
-    for (int j = 0; j < 5; j++)
+    private static void Main(string[] args)
     {
-        try
+        int[][] jaggedArr =
+        [
+          [1,2,3,4],
+          [2,2,3,33]
+        ];
+
+        string[][] fruits = { new string[] { "Apple", "Apricot" }, new string[] { "Mango", "Orange", "Melon" } };
+        for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine(jaggedArr[i][j] + " i={0}, j={1}; ", i, j); 
-        }
-        catch
-        {
-            Console.WriteLine("  Failed! i={0}, j={1}; ", i, j);
+            for (int j = 0; j < 5; j++)
+            {
+                try
+                {
+                    Console.WriteLine(jaggedArr[i][j] + " i={0}, j={1}; ", i, j);
+                }
+                catch
+                {
+                    Console.WriteLine("  Failed! i={0}, j={1}; ", i, j);
+                }
+            }
         }
     }
 }
