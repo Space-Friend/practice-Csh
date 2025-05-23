89edb1f16c7bcb4374078b109705b71cd8aba8e6
Space-Friend <Makashev_Leonid@mail.ru>
2025-05-23 11:56:27 +0300

    testing final test

---
+++ final-test-ig/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace final_test_ig
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "jquinjqwnqwi1ocnnx nqwxdnqn nqwoi noy ";
+            Console.WriteLine(a+"\n");
+            if (a.Contains("1"))
+            {
+                a = a[..a.IndexOf('1')];
+                Console.WriteLine(a);
+            }
+            
+        }
+    }
+}
