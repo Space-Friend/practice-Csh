c4eba6619522fd24978d2848df5fbbf9f2de4f69
Space-Friend <Makashev_Leonid@mail.ru>
2025-05-28 20:45:25 +0300

    update

--- from down to top/Program.cs
+++ from down to top/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            // Основные типы переменных
+            /*
+             * 
+             * Основные типы переменных
+             * 
+             */
             byte b;
             sbyte sb;
             short s;
@@ -24,7 +28,53 @@
 
             // Логические
             bool bo;
+            int first_num = 5;
+            int second_num = 2;
+            float x = Convert.ToSingle(first_num)/Convert.ToSingle(second_num);
+            Console.WriteLine(x);
 
+            /*
+             * 
+             * Урон
+             * 
+             */
+            int health = 100;
+            int armor = 1;
+            int damage;
+            Console.WriteLine("{0}hp. Введ" +
+                "ите урон: ", health);
+            //damage = Convert.ToInt16(Console.ReadLine());
+            damage = 100;
+            if (damage > armor)
+            {
+                health -= damage-armor;
+            }
+            Console.WriteLine(health);
+
+            /*
+             * 
+             * Еда
+             * 
+             */
+            int money = 100;
+            int food;
+            int foodPrice = 10;
+            bool isAbleToPay;
+            Console.WriteLine("Сколько еды?");
+            //food = Convert.ToInt16(Console.ReadLine());
+            food = 5;
+            
+            isAbleToPay = money >= food * food;
+            food *= Convert.ToInt32(isAbleToPay);
+            money -= food * foodPrice;
+            Console.WriteLine($"money {money}, food {food}");
+
+            if (isAbleToPay)
+            {
+                money -= food * foodPrice;
+                Console.WriteLine($"money {money}, food {food}");
+            }
+            Console.WriteLine();
         }
     }
 }
