def3b2fcbdd63a26aebf84ca3cb17612baaf3f2d
Space-Friend <Makashev_Leonid@mail.ru>
2025-05-28 19:47:00 +0300

    added new file

---
+++ from down to top/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace from_down_to_top
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Основные типы переменных
+            byte b;
+            sbyte sb;
+            short s;
+            ushort us;
+            int i;
+            uint ui;
+            long l;
+            ulong ul;
+
+            // С плавающей точкой
+            float f = 5.7f;
+            double d = 5.7;
+
+            //Символьные
+            char c = 'a';
+            string str = "string";
+
+            // Логические
+            bool bo;
+
+        }
+    }
+}
