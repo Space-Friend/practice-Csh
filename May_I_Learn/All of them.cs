5ed0db384a783038e70ecb108a9fa7fdac91cfc1
Space-Friend <Makashev_Leonid@mail.ru>
2026-05-01 21:36:09 +0300

    begin again

---
+++ May_I_Learn/All of them.cs
@@ -0,0 +1,10 @@
+﻿namespace May_I_Learn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
